438a9a086ac3da648070df72b15690eed203d825
codicate <codicate@gmail.com>
2021-02-24 18:03:04 -0500

    Fixed bugs, no more crash

--- script.js
+++ script.js
@@ -9,55 +9,60 @@ function isNumeric(num) {
 const getFactorial = (num) =>
     (num === 0)
         ? 1
-        : num * getFactorial(num - 1);
+        : (num > 0)
+            ? num * getFactorial(num - 1)
+            : num * getFactorial(num + 1);
 
 function compute(firstNum, operation, secondNum) {
+
+    console.log(firstNum);
+    console.log(operation);
+    console.log(secondNum);
+
+    let answer = 0;
     switch (operation) {
         case '÷':
-            return firstNum / secondNum;
+            answer = firstNum / secondNum;
+            break;
         case '×':
-            return firstNum * secondNum;
+            answer = firstNum * secondNum;
+            break;
         case '-':
-            return firstNum - secondNum;
+            answer = firstNum - secondNum;
+            break;
         case '+':
-            return firstNum + secondNum;
+            answer = firstNum + secondNum;
+            break;
     }
+    return Math.round(answer * Math.pow(10, 10)) / Math.pow(10, 10);
 }
 
-let numInput = [];
-let currentNum = '';
+let equation = [];
+let currentNum = '0';
+let lastNum = '0';
 let currentOperation = '';
 
 function getKeypress(element) {
 
     // numbers
-    if (element.className == 'number' && currentNum.length < 10) {
-        currentNum += element.textContent;
+    if (element.className === 'number') {
+        if (element.textContent === '.') {
+            if (currentNum.indexOf('.') === -1)
+                currentNum += element.textContent;
 
-        // operations like /*-+
-    } else if (element.className == 'operation') {
-        if (numInput.length === 0) {
-            numInput.push(currentNum, element.textContent);
-
-        } else if (currentOperation != '') {
-            
-            numInput[numInput.length - 1] = element.textContent;
-
-        } else {
-            console.log(element.textContent);
-            numInput.push(currentNum, '=', compute(parseFloat(numInput[numInput.length - 2]), numInput[numInput.length - 1], parseFloat(currentNum)));
-            if (element.textContent != '=') numInput.push(element.textContent);
+        } else if (currentNum.length < 10) {
+            if (currentNum === '0') currentNum = element.textContent;
+            else currentNum += element.textContent;
         }
 
-        equationDisplay.textContent = numInput.join(' ');
-        currentNum = '';
+        // functions that require calculation like ^! or AC/C
+    } else if (element.className === 'function') {
 
-        // functions that require calculation like =^! or AC/C
-    } else {
         switch (element.id) {
             case 'AC':
-                currentNum = '';
-                numInput = [];
+                currentNum = '0';
+                lastNum = '0';
+                equation = [];
                 equationDisplay.innerHTML = '&nbsp;';
                 break;
 
@@ -66,21 +71,43 @@ function getKeypress(element) {
                 break;
 
             case 'fact':
-                currentNum = getFactorial(parseInt(currentNum));
+                if (Math.abs(currentNum) < 170)
+                    currentNum = getFactorial(parseInt(currentNum));
+                else currentNum = Infinity;
                 break;
 
             case 'negate':
                 currentNum = parseInt(currentNum) * -1;
                 break;
         }
+
+        // operations like /*-+
+    } else {
+        if (lastNum !== '0' && currentNum !== '0') {
+            equation.push(currentNum, '=');
+            currentNum = compute(parseFloat(lastNum), currentOperation, parseFloat(currentNum)).toString();
+            lastNum = '0';
+        }
+
+        if (lastNum === '0' && element.textContent !== '=') {
+            currentOperation = element.textContent;
+            equation = [];
+            equation.push(currentNum, element.textContent);
+
+            lastNum = currentNum;
+            currentNum = '0';
+
+        } else if (element.textContent !== '=') {
+            equation[equation.length - 1] = element.textContent;
+        }
+
+        equationDisplay.textContent = equation.join(' ');
     };
 
     currentNum = currentNum.toString();
-    currentNumDisplay.textContent = currentNum == ''
-        ? '0'
-        : currentNum;
+    currentNumDisplay.textContent = currentNum;
 
-    console.log(numInput);
+    console.log(equation);
 }
 
 for (let btn of keys.children) {
